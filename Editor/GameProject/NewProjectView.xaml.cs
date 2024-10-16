3ef60f3e1fe58058c1b7465261877a069839da1b
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2024-10-17 00:14:33 +0100

    feat: [ARC-1] add basic setup

---
+++ Editor/GameProject/NewProjectView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Editor.GameProject
+{
+    public partial class NewProjectView : UserControl
+    {
+        public NewProjectView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Editor/MainWindow.xaml.cs
+++ Editor/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Editor.GameProject;
 
 namespace Editor
 {
@@ -23,6 +11,26 @@ namespace Editor
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectBrowserDialog();
+        }
+
+        private void OpenProjectBrowserDialog()
+        {
+            var projectBrowser = new ProjectBrowserDialog();
+            if (projectBrowser.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                
+            }
         }
     }
 }
